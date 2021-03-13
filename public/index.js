dc9883e0907bbde41ff755e0c94e94e0fc989295
Luis Felipe de Jesus <luisfelipedejesusm@gmail.com>
2021-03-13 19:06:01 -0400

    final

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+var CREATED = "El cliente ha sido agregado satisfactoriamente.";
 
 var getClienteFromWindow = function(){
     return new Cliente(
@@ -6,8 +7,43 @@ var getClienteFromWindow = function(){
     )
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    firebase.database().ref('clientes').on('value', (snapshot) => {
+        tableElem.clientes = snapshot.val();
+      });
+})
+
+var tableElem = new Vue({
+    el: "#table",
+    data: {
+        clientes: []
+    },
+    methods: {
+        deleteCliente: function(k){
+            firebase.database().ref('clientes/'+k).remove()
+        }
+    }
+})
+
+var agregarCliente = _ => {
+    saveCliente(
+        getClienteFromWindow()
+    )
+    clearFields()
+    showMsg(CREATED)
+}
+
+var showMsg = msg => {
+    alert(msg)
+}
+
+var clearFields = _ => {
+    document.getElementById("nombre-cliente").value = ""
+    document.getElementById("apellido-cliente").value = ""
+}
+
 var saveCliente = cliente => {
-    firebase.database().ref('users/' + userId).set(cliente)
+    firebase.database().ref('clientes').push().set(cliente)
 }
 
 var getElem = _ => document.getElementById(_).value
